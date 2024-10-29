67a023abff092808e3b3e0175757c381635d0f09
qtaghdi <135234546+qtaghdi@users.noreply.github.com>
2024-10-29 15:14:54 +0900

    Refactor :: Token upgrade

---
+++ src/constants/token/token.ts
@@ -0,0 +1,19 @@
+import Cookies from "js-cookie";
+import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from './token.constants'; // 상수 파일에서 가져오기
+
+class Token {
+    public getToken(key: string): string | undefined {
+        return Cookies.get(key);
+    }
+
+    public setToken(key: string, token: string, expires: number = 7): void {
+        Cookies.set(key, token, { expires : 2 }); // 토큰 만료일 수정
+    }
+
+    public clearToken() {
+        Cookies.remove(ACCESS_TOKEN_KEY);
+        Cookies.remove(REFRESH_TOKEN_KEY);
+    }
+}
+
+export default new Token();
