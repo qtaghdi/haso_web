353f2ba77c7bf8989c8609eb08f5930e08e4e5a6
박상민 <a01071397193@gmail.com>
2024-12-17 13:33:38 +0900

    Fix :: svg 에러 진짜 해결

--- @types/image.d.ts
+++ @types/image.d.ts
@@ -1,9 +1,8 @@
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
 declare module "*.png";
 declare module "*.jpg";
 declare module "*.jpeg";
-declare module "*.svg" {
-  import React from "react";
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
-}
 declare module "*.gif";
