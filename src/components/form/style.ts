24952431a24945bbf98d4bbb4641cce074ea1294
박상민 <a01071397193@gmail.com>
2024-12-26 12:30:39 +0900

    Style :: Form Border 수정

--- src/components/form/style.ts
+++ src/components/form/style.ts
@@ -4,7 +4,7 @@ export const Form = styled.form`
     width: 35vw;
     height: 70vh;
     padding: 1.563rem;
-    border: 5px solid;
+    border: 0.5vw solid;
     z-index: 1;
     border-radius: ${({ theme }) => theme.borderRadius.large};
     background-color: ${({ theme }) => theme.colors.white};
