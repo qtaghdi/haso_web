e571ecbfa44c727e5c857ad5c59cbf57bec34673
박상민 <a01071397193@gmail.com>
2024-12-30 15:49:51 +0900

    Feat :: js-Cookies -> localStorage

--- src/constants/token/token.ts
+++ src/constants/token/token.ts
@@ -1,18 +1,17 @@
-import * as Cookies from 'js-cookie'
 import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from './token.constants';
 
 class Token {
-    public getToken(key: string): string | undefined {
-        return Cookies.get(key);
+    public getToken(key: string): string | null {
+        return localStorage.getItem(key);
     }
 
-    public setToken(key: string, token: string, expires: number = 7): void {
-        Cookies.set(key, token, { expires : 2 }); // 토큰 만료일 수정
+    public setToken(key: string, token: string): void {
+        localStorage.setItem(key, token);
     }
 
-    public clearToken() {
-        Cookies.remove(ACCESS_TOKEN_KEY);
-        Cookies.remove(REFRESH_TOKEN_KEY);
+    public clearToken(): void {
+        localStorage.removeItem(ACCESS_TOKEN_KEY);
+        localStorage.removeItem(REFRESH_TOKEN_KEY)
     }
 }
 
