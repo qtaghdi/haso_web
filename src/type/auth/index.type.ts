32d3ff39dc7ff906b3b7a34cea6663063d477497
박상민 <a01071397193@gmail.com>
2024-12-31 17:36:27 +0900

    Feat :: auth 타입 추가

---
+++ src/type/auth/index.type.ts
@@ -0,0 +1,16 @@
+export interface SignUpProps {
+    name: string;
+    id: string;
+    password: string;
+    passwordConfirm: string; // 비밀번호 확인
+    tel: string;
+    telAccess: string; // 전화번호 인증 번호
+    business_name // 상호명
+    store_no: string;
+    fax_no: string;
+}
+
+export interface LoginProps {
+    id: string;
+    password: string;
+}
