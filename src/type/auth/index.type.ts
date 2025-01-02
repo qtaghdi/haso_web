5e0d746104384fe3a6319f0e6411d7addf52759b
박상민 <a01071397193@gmail.com>
2025-01-02 17:08:25 +0900

    Fix :: 상호명 값 포함되게 수정

--- src/type/auth/index.type.ts
+++ src/type/auth/index.type.ts
@@ -5,7 +5,7 @@ export interface SignUpProps {
     passwordConfirm: string; // 비밀번호 확인
     tel: string;
     telAccess: string; // 전화번호 인증 번호
-    business_name // 상호명
+    business_name: string; // 상호명
     store_no: string;
     fax_no: string;
 }
