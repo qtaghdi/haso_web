5aa2fc11c716c5dfc81c0cc58793321137cf337c
박상민 <a01071397193@gmail.com>
2025-01-04 13:04:01 +0900

    Feat :: 회원가입 1차 뷰 완성

--- src/page/auth/signup/index.tsx
+++ src/page/auth/signup/index.tsx
@@ -1,9 +1,117 @@
-const Signup = () => {
-    return (
-        <>
+import React, { useState } from "react";
+import * as S from "./style";
+import Form from "@/components/form";
+import { SIGNUP_STEPS } from "./steps";
+import { useNavigate } from "react-router-dom";
+import { SignUpProps } from "@/type/auth/index.type";
+import LargeButton from "@/components/button/large";
+import MiddleButton from "@/components/button/middle";
 
-        </>
-    )
-}
+const SignUp = () => {
+  const [currentStep, setCurrentStep] = useState(0);
+  const [formData, setFormData] = useState<SignUpProps>({
+    name: "",
+    id: "",
+    password: "",
+    passwordConfirm: "",
+    tel: "",
+    telAccess: "",
+    business_name: "",
+    store_no: "",
+    fax_no: "",
+  });
+  const navigate = useNavigate();
 
-export default Signup;
+  const CurrentStepComponent = SIGNUP_STEPS[currentStep]?.component;
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    const isValidField = (field: string): field is keyof SignUpProps => field in formData;
+
+    if (isValidField(name)) {
+      setFormData((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
+  };
+
+  const isCurrentStepValid = () => {
+    const { validate } = SIGNUP_STEPS[currentStep];
+    return validate ? validate(formData) : true;
+  };
+
+  const handleNext = () => {
+    if (currentStep < SIGNUP_STEPS.length - 1 && isCurrentStepValid()) {
+      setCurrentStep((prev) => prev + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentStep > 0) {
+      setCurrentStep((prev) => prev - 1);
+    }
+  };
+
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
+  return (
+    <S.Wrapper>
+      <S.BackGround>
+        <S.SecondaryCircle />
+        <S.PrimaryCircle />
+      </S.BackGround>
+      <Form>
+        <S.StepIndicator>
+          {SIGNUP_STEPS.map((_, index) => (
+            <React.Fragment key={index}>
+              <S.StepCircle active={currentStep >= index}>{index + 1}</S.StepCircle>
+              {index < SIGNUP_STEPS.length - 1 && <S.StepLine active={currentStep > index} />}
+            </React.Fragment>
+          ))}
+        </S.StepIndicator>
+
+        <S.TitleBox>
+          <S.Title>계정 생성하기</S.Title>
+          <S.Description>먼저 사용자 정보를 입력해주세요.</S.Description>
+        </S.TitleBox>
+
+        <S.FieldWrapper>
+          {CurrentStepComponent ? (
+            <CurrentStepComponent signUp={formData} onChange={handleInputChange} />
+          ) : (
+            <div>올바르지 않은 단계입니다.</div>
+          )}
+        </S.FieldWrapper>
+
+        <S.LoginSection>
+          <S.ButtonSection>
+            {currentStep > 0 && <MiddleButton variant="secondary" onClick={handlePrev}>이전</MiddleButton>}
+            {currentStep === 0 ? ( // Step이 One일 경우
+              <LargeButton onClick={handleNext} disabled={!isCurrentStepValid()}>
+                다음
+              </LargeButton>
+            ) : (
+              currentStep < SIGNUP_STEPS.length - 1 ? (
+                <MiddleButton variant="primary" onClick={handleNext} disabled={!isCurrentStepValid()}>
+                  다음
+                </MiddleButton>
+              ) : (
+                <LargeButton /*onClick={handleSubmit}*/ disabled={!isCurrentStepValid()}>
+                  완료
+                </LargeButton>
+              )
+            )}
+          </S.ButtonSection>
+          <S.Footer>
+            계정이 있으신가요? <S.SignUpLink onClick={handleLogin}>로그인하기</S.SignUpLink>
+          </S.Footer>
+        </S.LoginSection>
+      </Form>
+    </S.Wrapper>
+  );
+};
+
+export default SignUp;
--- src/page/auth/signup/style.ts
+++ src/page/auth/signup/style.ts
@@ -1,2 +1,144 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
+export const Wrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: ${({ theme }) => theme.colors.gray[200]};
+    border-radius: ${({ theme }) => theme.borderRadius.large};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+`;
+
+
+// 배경
+export const BackGround = styled.div`
+    position: absolute;
+    width: 70%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: ${({ theme }) => theme.colors.gray[200]};
+`;
+
+export const PrimaryCircle = styled.div`
+    position: absolute;
+    width: 35vw;
+    height: 35vw;
+    background: rgba(50, 125, 255, 0.7);
+    filter: blur(15px);
+    border-radius: 100%;
+    bottom: 5%;
+    right: 5%;
+`;
+
+export const SecondaryCircle = styled.div`
+    position: absolute;
+    width: 35vw;
+    height: 35vw;
+    background: rgba(79, 209, 197, 0.7);
+    filter: blur(15px);
+    border-radius: 100%;
+    left: 5%;
+    top: 5%;
+`;
+
+
+// 상단
+export const TitleBox = styled.div`
+    display: flex;
+    padding: 10px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 0.4rem;
+`
+
+export const Title = styled.h1`
+    font-size: ${({ theme }) => theme.typography.fontSizes.title[2]};
+    font-family: "Pretendard-Bold";
+`;
+
+export const Description = styled.p`
+    font-family: "Pretendard-Medium";
+    font-size: ${({ theme }) => theme.typography.fontSizes.body[1]};
+    color: ${({ theme }) => theme.colors.text.Light};
+    margin-bottom: 30px;
+`;
+
+
+// 중단
+export const FieldWrapper = styled.div`
+    display: flex;
+    padding: 0.625rem;
+    flex-direction: column;
+    gap: 10px;
+`
+
+export const StepIndicator = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 20px 0;
+`;
+
+export const StepCircle = styled.div<{ active: boolean }>`
+    width: 30px;
+    height: 30px;
+    background-color: ${({ active, theme }) => (active ? theme.colors.primary[400] : theme.colors.gray[300])};
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    font-weight: bold;
+    transition: background-color 0.3s, transform 0.3s;
+    ${({ active }) => active && `
+        transform: scale(1.2);
+    `}
+`;
+
+
+export const StepLine = styled.div<{ active: boolean }>`
+    width: 50px;
+    height: 2px;
+    background-color: ${({ active, theme }) => (active ? theme.colors.primary[400] : theme.colors.gray[300])};
+    transition: background-color 0.3s;
+`;
+
+
+// 하단
+export const LoginSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 0.625rem;
+    align-items: center;
+    justify-content: center;
+    gap: 1.2rem;
+`;
+
+export const ButtonSection = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+`
+
+export const Footer = styled.div`
+    font-family: "Pretendard-Medium";
+    font-size: ${({ theme }) => theme.typography.fontSizes.body[2]};
+`;
+
+export const SignUpLink = styled.a`
+    font-family: "Pretendard-SemiBold";
+    color: ${({theme}) => theme.colors.primary[400]};
+    text-decoration: none;
+
+    &:hover {
+        text-decoration: underline;
+        cursor: pointer;
+    }
+`;
