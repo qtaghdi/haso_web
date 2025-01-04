aea4a738361da4cb75e59a0110d981775b8d7f72
박상민 <a01071397193@gmail.com>
2025-01-05 00:13:21 +0900

    Style :: Preetier에 맞춰서 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import {ThemeProvider} from "styled-components";
-import {theme} from "@/design/theme";
+import { ThemeProvider } from "styled-components";
+import { theme } from "@/design/theme";
 import Router from "@/routes";
 import GlobalStyle from "@/design/GlobalStyle";
 
 function App() {
-    return (
-        <ThemeProvider theme={theme}>
-            <GlobalStyle/>
-            <Router/>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router />
+    </ThemeProvider>
+  );
 }
 
 export default App;
