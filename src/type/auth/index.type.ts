cc9b130762ff2478a911375022e22b1ef21ecc3c
박상민 <a01071397193@gmail.com>
2025-01-06 01:00:53 +0900

    Feat :: api 변경에 따른 타입 변경

--- src/type/auth/index.type.ts
+++ src/type/auth/index.type.ts
@@ -5,12 +5,19 @@ export interface SignUpProps {
     passwordConfirm: string; // 비밀번호 확인
     tel: string;
     telAccess: string; // 전화번호 인증 번호
-    business_name: string; // 상호명
-    store_no: string;
-    fax_no: string;
+    store_name: string; // 상호명
+    store_no: string; // 사업장 번호
+    fax_no: string; // 팩스 번호
+    business_no: string; // 사업자 등록 번호
 }
 
 export interface LoginProps {
     id: string;
     password: string;
+}
+
+export interface ValidateProps extends SignUpProps {
+    b_no: string; // 사업자 등록 번호
+    start_dt: string; // 개업일자
+    p_nm: string; // 대표자 성명
 }
