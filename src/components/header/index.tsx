99cc959fb38a803da1a80baa2b14427445f797a3
박상민 <a01071397193@gmail.com>
2025-01-20 01:18:54 +0900

    Fix :: Logo 위치 수정

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,19 +1,10 @@
-import React from "react";
 import * as S from "./style";
-import Logo from "@/components/logo/Logo.svg";
+import Logo from "@/components/icons/logo"
 
 const Header = () => {
   return (
     <S.Wrapper>
-      <S.Container>
-        <Logo />
-        <S.SearchBar placeholder="찾으시는게 있으신가요?" />
-        <S.NavBar>
-          <span>상품 등록</span>
-          <span>채팅</span>
-          <span>거래처 관리</span>
-        </S.NavBar>
-      </S.Container>
+      
     </S.Wrapper>
   );
 };
