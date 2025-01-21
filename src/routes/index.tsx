ae6f6bd6dae8c9d685a57d08e040cea16b9f78eb
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2025-01-21 18:09:32 +0900

    Feat :: 경로 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,13 @@ import { paths } from "@/constants/path";
 import Login from "@/page/auth/login";
 import SignUp from "@/page/auth/signup";
 import Main from "@/page/main";
+import ClientList from "@/page/manage/clientList";
+import BusinessHistory from "@/page/manage/businessHistory";
+import Statement from "@/page/manage/statement";
+import ProviderPage from "@/page/manage/provider";
+import ReceiverPage from "@/page/manage/receiver";
+import AddClient from "@/page/manage/addClient";
+import AddBusiness from "@/page/manage/addBusiness";
 
 const Router = () => {
   return (
@@ -11,6 +18,13 @@ const Router = () => {
         <Route path={paths.login} element={<Login />} />
         <Route path={paths.signup} element={<SignUp />} />
           <Route path={paths.main} element={<Main />} />
+          <Route path={paths.clientList} element={<ClientList/>} />
+          <Route path={paths.businessHistory} element={<BusinessHistory/>} />
+          <Route path={paths.statement} element={<Statement/>} />
+          <Route path={paths.statementProvider} element={<ProviderPage/>} />
+          <Route path={paths.statementReceiver} element={<ReceiverPage/>} />
+          <Route path={paths.addClient} element={<AddClient/>} />
+          <Route path={paths.addBusiness} element={<AddBusiness/>} />
       </Routes>
     </BrowserRouter>
   );
