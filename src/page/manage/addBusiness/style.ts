5d091e40d8a5333ab02f25e6e415567b2580bc4c
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2025-01-21 21:10:15 +0900

    Feat :: 거래내역 추가(기능명세서 작성) 페이지 뷰 짜기

---
+++ src/page/manage/addBusiness/style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const AddBusiness = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 190vh;
+  background-color: #F8F8F8;
+  gap: 4.5vh;
+`;
+export const AddBusinessTitle = styled.span`
+    margin-top: 13vh;
+  font-size: 40px;
+  font-weight: bold;
+
+`;
